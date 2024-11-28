22e656bc5aa3cb1e4ce6d3777ee88a28c499a1c5
angie1105 <zafimenaangelica@gmail.com>
2024-11-28 10:54:35 +0100

    master

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Roles } from "src/utility/common/user-roles.enum";
+import{ Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+Entity();
+
+@Entity('users')
+export class UserEntity{
+@Column()
+id:number;
+@Column()
+nom:string;
+@Column()
+adresse:string;
+@Column()
+telephone:number;
+@Column()
+password:string;
+@Column({type:'enum',enum:Roles,array:true,default:[Roles.USER]})
+roles:Roles[]
+}
+export class User {}
